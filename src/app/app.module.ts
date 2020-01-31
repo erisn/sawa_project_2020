6b5126eafb158fca8a1acf891775809ff9b86b09
Ghebremedhine Senai <senaisenai13@gmail.com>
2020-01-30 19:24:16 -0700

    created trainee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TraineeComponent } from './trainee/trainee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TraineeComponent
   ],
   imports: [
     BrowserModule
