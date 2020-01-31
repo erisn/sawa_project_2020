238c9a623db095b5e58a8b121045082776173beb
Ghebremedhine Senai <senaisenai13@gmail.com>
2020-01-30 20:28:38 -0700

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,39 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { HOMEComponent } from './home/home.component';
 import { MilitaryComponent } from './military/military.component';
-=======
-<<<<<<< HEAD
+
 import { TrainerComponent } from './trainer/trainer.component';
-=======
+
 import { TraineeComponent } from './trainee/trainee.component';
-<<<<<<< HEAD
 import { HealthComponent } from './health/health.component';
-=======
->>>>>>> 6b5126eafb158fca8a1acf891775809ff9b86b09
->>>>>>> 38935f7bd4da932868da749b0bba1091401c39b9
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     TraineeComponent,
     HealthComponent,
-    MilitaryComponent
-=======
-    HOMEComponent
->>>>>>> 58bc91d7b90f2b62f6d5d089e592b93bc374bdc1
-=======
-<<<<<<< HEAD
-    TrainerComponent
-=======
+    MilitaryComponent,
+    HOMEComponent,
+    TrainerComponent,
     TraineeComponent
->>>>>>> 6b5126eafb158fca8a1acf891775809ff9b86b09
->>>>>>> 38935f7bd4da932868da749b0bba1091401c39b9
-  ],
+ ],
   imports: [
     BrowserModule
   ],
