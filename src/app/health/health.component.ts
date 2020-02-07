408abd1cb84549beaeb10680bcc5c5c76973371a
Ghebremedhine Senai <senaisenai13@gmail.com>
2020-02-06 19:05:50 -0700

    created healthregistration

--- src/app/health/health.component.ts
+++ src/app/health/health.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-health',
   templateUrl: './health.component.html',
   styleUrls: ['./health.component.css']
 })
 export class HealthComponent implements OnInit {
+
+
+  firstName: string;
+  lastName: string;
+  userName: string;
+  phoneNumber: string;
 
   constructor() { }
 
